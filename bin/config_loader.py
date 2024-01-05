7d9cd2ae88dfcec8f035c27bc18277582295e91a
yelanyanyu <yelanyanyu@outlook.com>
2024-01-05 23:10:07 +0800

    v1.0.0

--- bin/config_loader.py
+++ bin/config_loader.py
@@ -1,11 +1,31 @@
 import json
 
+settings_path = '../config/settings.json'
+
+dns_server_path = '../config/dns_server.json'
+
 
 def read_default_settings():
-    with open('../config/settings.json') as f:
+    with open(settings_path, encoding='utf-8') as f:
         return json.load(f)
 
 
 def read_default_dns_server():
-    with open('../config/dns_server.json') as f:
+    with open(dns_server_path, encoding='utf-8') as f:
         return json.load(f)
+
+
+class NetConfig(object):
+    interfaces = []
+    dns_servers = []
+
+    def __init__(self):
+        self.interfaces = read_default_settings()['default_interfaces_name']
+        set_map = read_default_dns_server()
+        self.dns_servers.append(set_map['primary'])
+        self.dns_servers.append(set_map['secondary'])
+        for server in set_map['others']:
+            self.dns_servers.append(server)
+
+
+net_config = NetConfig()
--- bin/net_state.py
+++ bin/net_state.py
@@ -2,45 +2,16 @@ import subprocess
 import re
 import psutil
 import sys
+import config_loader
 
 
-class Net(object):
-    def __init__(self, net_name, description, mac_address,
-                 is_dhcp, is_autoconfig, ipv6_address, subnet_mask,
-                 gateway, dhcp_server, dns_server, net_bios):
-        self.net_name = net_name
-        self.description = description
-        self.mac_address = mac_address
-        self.is_dhcp = is_dhcp
-        self.is_autoconfig = is_autoconfig
-        self.ipv6_address = ipv6_address
-        self.subnet_mask = subnet_mask
-        self.gateway = gateway
-        self.dhcp_server = dhcp_server
-        self.dns_server = dns_server
-        self.net_bios = net_bios
-
-    def __str__(self):
-        return f"Network Name: {self.net_name}\n" \
-               f"Description: {self.description}\n" \
-               f"MAC Address: {self.mac_address}\n" \
-               f"DHCP Enabled: {self.is_dhcp}\n" \
-               f"Autoconfiguration Enabled: {self.is_autoconfig}\n" \
-               f"IPv6 Address: {self.ipv6_address}\n" \
-               f"Subnet Mask: {self.subnet_mask}\n" \
-               f"Default Gateway: {self.gateway}\n" \
-               f"DHCP Server: {self.dhcp_server}\n" \
-               f"DNS Server: {self.dns_server}\n" \
-               f"NetBIOS over Tcpip Enabled: {self.net_bios}\n"
-
-
-def get_connected_interfaces():
-    # Run the command to get all network interfaces
-    command = 'netsh interface show interface'
-    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-    # Find all connected interfaces
-    connected_interfaces = re.findall(r'已连接\s+专用\s+(以太网|WLAN)[^\r\n]*', result.stdout)
-    return [iface.split()[-1] for iface in connected_interfaces]
+# def get_connected_interfaces():
+#     # Run the command to get all network interfaces
+#     command = 'netsh interface show interface'
+#     result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+#     # Find all connected interfaces
+#     connected_interfaces = re.findall(r'已连接\s+专用\s+(以太网|WLAN)[^\r\n]*', result.stdout)
+#     return [iface.split()[-1] for iface in connected_interfaces]
 
 
 def get_all_interfaces_name():
@@ -71,16 +42,8 @@ def do_set_dns():
     Truly set the dns server for current user.
     """
     # Get the list of connected interfaces
-    interfaces = get_connected_interfaces()
+    interfaces = config_loader.net_config.interfaces
 
-    # Check the number of connected interfaces
-    if len(interfaces) > 1 or len(interfaces) < 0:
-        print("Error: The number of connected networks should be zero or one.")
-        sys.exit(1)
-    elif len(interfaces) == 0:
-        print("No connected network found. Exiting program.")
-        sys.exit(0)
-    else:
-        # Set DNS for the single connected interface
-        dns_servers = ["223.5.5.5", "10.1.1.9", "223.6.6.6"]
-        set_dns(interfaces[0], dns_servers)
+    # Set DNS for the single connected interface
+    dns_servers = config_loader.net_config.dns_servers
+    set_dns(interfaces[0], dns_servers)
